cb51c5f49e764bee2b6f334ac5d19b5015e6f8e0
hvriza39-rgb <hvriza39@gmail.com>
2026-06-11 09:18:16 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
+  interactiveWidget: "resizes-content",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
